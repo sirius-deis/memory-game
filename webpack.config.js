579a575b1daf17d18e3eb075eb466d312154be5e
Sirius Deis <falebsy@gmail.com>
2023-04-09 00:24:32 +0200

    finish webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    devtool: "source-map",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+            filename: "index.html",
+            title: "Memory game",
+            inject: "body",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: "public/",
+                },
+            ],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+        compress: true,
+        port: 3000,
+    },
+};
