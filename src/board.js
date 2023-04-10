fc6a79bae7cb996d9808dac2bfb9d9182578bcd2
Sirius Deis <falebsy@gmail.com>
2023-04-10 04:36:05 +0200

    add actions to buttons for chosing difficulty

--- src/board.js
+++ src/board.js
@@ -1,1 +1,16 @@
-const formBoard = () => {};
+const options = {
+    easy: [3, 3],
+    medium: [7, 5],
+    difficult: [15, 6],
+};
+
+export const createBoard = (set, difficulty) => {
+    const chosenDifficulty = options[difficulty];
+    const amountOfCells = chosenDifficulty[0] * chosenDifficulty[1];
+    const cardsSet = chooseCardsFromSet(set, amountOfCells / 2);
+};
+
+const chooseCardsFromSet = (set, amount) => {
+    const arrOfSet = set.split(" ");
+    console.log(arrOfSet);
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,68 @@
-fetch("/public/animals.txt")
-    .then((response) => response.text())
-    .then((data) => {
-        // const arr = data.split(" ");
-        // const el = document.querySelector(".board");
-        // console.log(el);
-        // el.innerHTML = arr.join(" ");
+import { createBoard } from "./board";
+
+const boardEl = document.querySelector(".board"),
+    btnPanel = document.querySelector(".btn__panel"),
+    easyBtn = document.querySelector(".easyBtn"),
+    mediumBtn = document.querySelector(".mediumBtn"),
+    difficultBtn = document.querySelector(".difficultBtn"),
+    resetBtn = document.querySelector(".resetBtn");
+
+let emojiList = "",
+    difficulty = "";
+
+const resetPanel = () => {
+    btnPanel.classList.remove("focus");
+    [easyBtn, mediumBtn, difficultBtn].forEach((btn) => {
+        btn.textContent = btn.dataset.name;
+        btn.removeAttribute("data-chosen");
+        btn.classList.remove("chosen");
     });
+};
+
+const resetBoard = () => {
+    boardEl.classList.remove(`board__${difficulty}`);
+};
+
+const focusOnBtnPanel = () => {
+    btnPanel.classList.add("focus");
+};
+
+const hideDifficultyButtons = () => {
+    [easyBtn, mediumBtn, difficultBtn].forEach((btn) => {
+        btn.classList.add("hidden");
+    });
+};
+
+const showResetBtn = () => {
+    resetBtn.classList.remove("hidden");
+};
+
+[easyBtn, mediumBtn, difficultBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.dataset.chosen) {
+            resetBoard();
+            difficulty = e.target.dataset.name;
+            boardEl.classList.add(`board__${difficulty}`);
+            hideDifficultyButtons();
+            showResetBtn();
+        } else {
+            resetPanel();
+            e.target.dataset.chosen = true;
+            e.target.textContent = "start";
+            e.target.classList.add("chosen");
+            focusOnBtnPanel();
+        }
+    });
+});
+
+const fetchEmojiList = async (url) => {
+    const response = await fetch(url);
+    const data = await response.text();
+    emojiList = data;
+};
+
+const start = () => {
+    createBoard(emojiList, "easy");
+};
+
+fetchEmojiList("/public/animals.txt");
